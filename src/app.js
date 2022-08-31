ae6fa228f4f76f97b1f3b88dc0d3787fc41d7cfd
goriens <sohaibgoriens@gmail.com>
2022-08-31 18:15:53 +0530

    final

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./Utils/Error");
 const user = require("./Routes/userRoutes");
-
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 const app = express();
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
